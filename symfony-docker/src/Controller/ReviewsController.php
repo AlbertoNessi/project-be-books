a4048996ab5d5077ebd7cb5fe312685c7fde52f9
alberto.nessi <albertonessi1@gmail.com>
2025-11-30 17:14:52 +0100

    Update and delete review

--- symfony-docker/src/Controller/ReviewsController.php
+++ symfony-docker/src/Controller/ReviewsController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use App\Entity\UserReviews;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
+use Symfony\Component\HttpFoundation\Response;
 
 class ReviewsController extends AbstractController 
 {
@@ -32,54 +33,76 @@ class ReviewsController extends AbstractController
 		]);
 	}
 
-	#[Route('/review', name: 'postReview_url', methods:['POST'])]
-	public function postReview(Request $request) {
+	#[Route(
+		'/review/{id}', 
+		name: 'handleReview_url', 
+		defaults: ['id' => '0'], 
+		methods:['POST', 'GET', 'PUT', 'DELETE']
+	)]
+	public function postReview(Request $request, $id) {
 		$parameters = $request->request->all();
-
-		if(!$parameters) {
-			return new JsonResponse("Parameters empty");
-		}
-
+		
 		try {
-			$newReview = new UserReviews();
-			$newReview->setBookId($parameters['book_id']);
-			$newReview->setBookTitle($parameters['book_title']);
-			$newReview->setReviewDescription($parameters['review_description']);
-			$newReview->setReviewVote($parameters['review_vote']);
-
-			$this->entityManager->persist($newReview);
-			$this->entityManager->flush();
-
-
-			return new JsonResponse([
-				'new_review_id' => $newReview->getId(),
-				'new_review_vote' => $newReview->getReviewVote()
-			]);
-		} catch (Exception $ex) {
-			return new JsonResponse($ex->getMessage(), $ex->getTraceAsString());
-		}
-	}
-
-	#[Route('/review/{reviewId}', name: 'getReviewById_url', methods:['GET'])]
-	public function getReviewById(Request $request, $reviewId) {
-		$parameters = $request->request->all();
-
-		if(!$reviewId) {
-			return new JsonResponse("No review id given");
-		}
-
-		try {
-			$review = $this->entityManager->getRepository(UserReviews::class)->find($reviewId);
+			$review = $this->entityManager->getRepository(UserReviews::class)->find($id);
 			$reviewData = [];
-			if($review) {
-				$reviewData = [
-					'book_title' => $review->getBookTitle(),
-					'review_vote' => $review->getReviewVote(),
-					'review_description' => $review->getReviewDescription()
-				];
-			}
+
+			if ($request->isMethod('POST')) {
+				if($review) {
+					throw new Exception("Review already exists with this id: $id", Response::HTTP_INTERNAL_SERVER_ERROR);
+				}
 	
-			return new JsonResponse($reviewData);
+				$newReview = new UserReviews();
+				$newReview->setBookId($parameters['book_id']);
+				$newReview->setBookTitle($parameters['book_title']);
+				$newReview->setReviewDescription($parameters['review_description']);
+				$newReview->setReviewVote($parameters['vote']);
+	
+				$this->entityManager->persist($newReview);
+				$this->entityManager->flush();
+	
+				return new JsonResponse([
+					'new_review_id' => $newReview->getId(),
+					'new_review_vote' => $newReview->getReviewVote()
+				]);
+			} elseif ($request->isMethod('GET')) {	
+				if($review) {
+					$reviewData = [
+						'book_title' => $review->getBookTitle(),
+						'review_vote' => $review->getReviewVote(),
+						'review_description' => $review->getReviewDescription()
+					];
+				}
+		
+				return new JsonResponse($reviewData);
+			} elseif ($request->isMethod('PUT')) {
+				if(!$review) {
+					throw new Exception("Review not found with id: $id", Response::HTTP_NOT_FOUND);
+				}
+				
+				$review->setReviewVote($parameters['vote']);
+				$review->setReviewDescription($parameters['review_description']);
+
+				$this->entityManager->persist($review);
+				$this->entityManager->flush();
+	
+				return new JsonResponse([
+					'new_review_id' => $review->getId(),
+					'new_review_vote' => $review->getReviewVote(),
+					'new_review_description' => $review->getReviewDescription()
+				]);
+
+				return new JsonResponse($reviewData);
+			} elseif ($request->isMethod('DELETE')) {
+				$review = $this->entityManager->getRepository(UserReviews::class)->find($id);
+				$reviewData = [];
+				
+				if($review) {
+					$this->entityManager->remove($review);
+					$this->entityManager->flush();
+				}	
+
+				return new JsonResponse();
+			}	
 		} catch (Exception $ex) {
 			return new JsonResponse($ex->getMessage(), $ex->getTraceAsString());
 		}
